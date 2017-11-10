af5c0b70fc82eb5e93fa27475ec8ee130d0d9d5e
Arman Eshaghi <armaneshaghi@users.noreply.github.com>
2017-11-10 16:36:39 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 DATA_DIR=${PWD}
 OUT_DIR=$DATA_DIR/Output
-#check if ANTSPATH is set
+##check if ANTSPATH is set
 : ${ANTSPATH:?"Need to set ANTSPATH non-empty"}
 #assusming that ANTSPATH is already set and in the $PATH
 # LesionFilling imageDimension T1_image.nii.gz lesion_mask.nii.gz output_lesion_filled.nii.gz
